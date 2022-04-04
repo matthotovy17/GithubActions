fa5754244763da65dc634439fe1e8472e5510d33
Matt Hotovy <42526675+matthotovy17@users.noreply.github.com>
2022-04-04 08:56:47 -0500

    Fixed failing unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -36,7 +36,7 @@ namespace GithubActionsLab
         [Test]
         public void Subtract_Valid()
         {
-            Assert.AreEqual(1, Program.Subtract("12", "2")); //Should be 10
+            Assert.AreEqual(10, Program.Subtract("12", "2")); //Should be 10
             Assert.AreEqual(1, Program.Subtract("3", "2"));
             Assert.AreEqual(-2, Program.Subtract("5", "7"));
         }
